33ff4bce361a5ceb73207df58c6896e11e92df6a
zhangruyi <990198301@qq.com>
2017-08-24 17:00:29 +0800

    水果防御

--- DIYPyGame/source/weight.py
+++ DIYPyGame/source/weight.py
@@ -1,13 +1,12 @@
-import sys,pygame
-
+import sys, pygame
+from pygame.locals import *
 from random import randrange
 
 class Weight(pygame.sprite.Sprite):
     
-    
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = self.weight_image
+        self.image = pygame.image.load("weight.jpg").convert()
         self.rect = self.image.get_rect()
         self.reset()
         
@@ -15,23 +14,23 @@ class Weight(pygame.sprite.Sprite):
     def reset(self):
         ""
         self.rect.top = -self.rect.height
-        self.rect.centerx = randrange(self.screen_size[0])
+        self.rect.centerx = randrange(screen_size[0])
         
     def update(self):
         self.rect.top += 1
         if self.rect.top > self.sceen_size[1]:
             self.reset()
-
+        
     pygame.init()
     sceen_size = 800, 600
     pygame.display.set_mode(sceen_size,pygame.locals.FULLSCREEN)
     pygame.mouse.set_visible(0)
     
-    weight_image = pygame.image.load("DIYPyGame/source/picture/weight.png")
-    weight_image = weight_image.convert()
+    
     
     sprites = pygame.sprite.RenderUpdates()
-    #sprites.add(Weight())
+    sprites.add(Weight())
+    
     
     screen = pygame.display.get_surface()
     bg = (255,255,255)
@@ -43,9 +42,9 @@ class Weight(pygame.sprite.Sprite):
     
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.event.QUIT:
+            if event.type == QUIT:
                 sys.exit()
-            if event.type == pygame.event.KEYDOWN and event.type == pygame.event.K_ESCAPE:
+            if event.type == KEYDOWN and event.type == K_ESCAPE:
                 sys.exit()
                 
     sprites.clear(screen,clear_callback)
